7ea70a0f7fc0360ac2f00bc6e3a75d4ddcbb2548
yajuna <yajun.an@gmail.com>
2019-06-11 11:56:12 -0700

    adding first python file

---
+++ python/pythonLab0.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 11 11:37:50 2019
+
+@author: yajun
+"""
+
+import numpy as np
+
+# mod operator %
+
+123456%789
+
+# complex numbers
+
+np.angle(1+1j*np.sqrt(2))
